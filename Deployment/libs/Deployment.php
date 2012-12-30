a80f8f5b39aa2fc7ba992f18fa161f873a246dbc
Filip Procházka <hosiplan@gmail.com>
2012-12-30 02:10:29 +0100

    After upload, delete files smartly

--- Deployment/libs/Deployment.php
+++ Deployment/libs/Deployment.php
@@ -281,11 +281,7 @@ class Deployment
 		foreach ($files as $num => $file) {
 			$remoteFile = $root . $file;
 			$this->writeProgress($num + 1, count($files), "Deleting $file");
-			if (substr($file, -1) === '/') { // is directory?
-				$this->ftp->rmdir($remoteFile);
-			} else {
-				$this->ftp->delete($remoteFile);
-			}
+			$this->ftp->deleteRecursive($remoteFile);
 		}
 	}
 
